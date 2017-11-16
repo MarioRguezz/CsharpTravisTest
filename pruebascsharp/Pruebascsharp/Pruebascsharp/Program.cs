52e4df0107f7c0fb4f719c0634a51968e4f37400
Mario Negrete Rodríguez <rasmus_darkmariozp@hotmail.com>
2017-11-16 17:13:46 -0600

    fibonacci test style cop

---
+++ pruebascsharp/Pruebascsharp/Pruebascsharp/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+namespace Pruebascsharp
+{
+	public class MainClass
+	{
+		//fibonacci code
+		public int Fibonacci(int n)
+		{
+			if (n == 0 || n == 1)
+			{
+				return n;
+			}
+			else
+			{
+				return Fibonacci(n - 1) + Fibonacci(n - 2);
+			}
+		}
+
+		//main method
+		public static void Main(string[] args)
+		{
+			MainClass fib = new MainClass();
+			Console.WriteLine(fib.Fibonacci(20));
+		}
+	}
+}
